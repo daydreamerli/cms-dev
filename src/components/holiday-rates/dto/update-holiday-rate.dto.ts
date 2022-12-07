3f64890de58495aa3bb6f2eeb62eb8bcfc94f07f
daydreamerli <frankli84@outlook.com>
2022-12-07 23:48:42 +1300

    remove old modules

--- src/components/holiday-rates/dto/update-holiday-rate.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateHolidayRateDto } from './create-holiday-rate.dto';
-
-export class UpdateHolidayRateDto extends PartialType(CreateHolidayRateDto) {}
--- src/components/owners/owners.module.ts
+++ src/components/owners/owners.module.ts
@@ -3,10 +3,11 @@ import { OwnersService } from './owners.service';
 import { OwnersController } from './owners.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Owner } from './entities/owner.entity';
+import { OwnerProfile } from './entities/owner-profile.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Owner])],
+  imports: [TypeOrmModule.forFeature([Owner, OwnerProfile])],
   controllers: [OwnersController],
-  providers: [OwnersService]
+  providers: [OwnersService],
 })
 export class OwnersModule {}
